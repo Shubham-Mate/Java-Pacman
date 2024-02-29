d577c4576adaa46b948150f1755b599c5e250505
Shubham-Mate <73774843+Shubham-Mate@users.noreply.github.com>
2024-02-29 15:36:49 +0530

    Add files via upload

--- Level.java
+++ Level.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 
 public class Level {
     File levelFile;
@@ -31,5 +34,13 @@ public class Level {
 
     int[][] getLevel() {
         return this.levelMap;
+    }
+
+    void drawMap(Graphics g, BufferedImage[] tileset, int TILE_HEIGHT, int TILE_WIDTH) {
+        for (int i=0; i<25; i++) {
+            for (int j=0; j<40; j++) {
+                g.drawImage(tileset[levelMap[i][j]], TILE_WIDTH*j, TILE_HEIGHT*i, TILE_WIDTH, TILE_HEIGHT, null);
+            }
+        }
     }
 }
